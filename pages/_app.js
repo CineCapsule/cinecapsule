af97efac1c1c0ad086010f15420e039084c90fec
marjorie <marjorie.ngoupende@gmail.com>
2022-06-09 15:39:13 +0200

    modifications

--- pages/_app.js
+++ pages/_app.js
@@ -6,11 +6,14 @@ import Footer from "./api/footer";
 function Marketplace({ Component, pageProps }) {
   return (
     <div>
+      <helmet>
+        <title>Marketplace NFTs</title>
+        <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+      </helmet>
       <nav className="border-b p-6 bg-black">
         <p className="text-4xl font-bold text-white">CineCapsule 3.0</p>
         <p className="text-2xl mb-4 text-gray-400">
-        Marketplace to access to film production through blockchain
-{" "}
+          Marketplace to access to film production through blockchain{" "}
         </p>
         <div className="flex mt-4">
           <Link href="/">
@@ -33,13 +36,12 @@ function Marketplace({ Component, pageProps }) {
       </nav>
       <br />
       <h1 className=" text-center text-6xl font-normal leading-normal mt-0 mb-2 text-red-700">
-      Films from future 🍿
+        Films from future 🍿
       </h1>
       <br />
       <hr className="border-b border-yellow-400 w-1/2 mx-auto" />
       <br />
-       <div className="flex justify-evenly">
-      </div>
+      <div className="flex justify-evenly"></div>
       <Component {...pageProps} />
       <Footer />
     </div>
